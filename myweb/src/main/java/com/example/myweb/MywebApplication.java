9d0b144f807095d0c3c479130ac98c539fd4258e
a <a>
2022-06-25 12:48:38 +0900

    java 1일차

---
+++ myweb/src/main/java/com/example/myweb/MywebApplication.java
@@ -0,0 +1,17 @@
+package com.example.myweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MywebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MywebApplication.class, args);
+		System.out.println("myweb 실행 완료");
+	}
+
+}
+
+// 프론트(화면) : html/css/javascript
+// 백(기능) : java
