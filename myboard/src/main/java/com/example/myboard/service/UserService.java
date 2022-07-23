293801e08d4c015e75e9480b5147fc68077a6f69
a <a>
2022-07-23 12:45:37 +0900

    자바9

---
+++ myboard/src/main/java/com/example/myboard/service/UserService.java
@@ -0,0 +1,8 @@
+package com.example.myboard.service;
+
+import java.util.List;
+import com.example.myboard.model.User;
+
+public interface UserService {
+	List<User> getAllUser();			// {} 없음
+}
---
+++ myboard/src/main/java/com/example/myboard/service/impl/UserServiceImpl.java
@@ -0,0 +1,28 @@
+package com.example.myboard.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.myboard.model.User;
+import com.example.myboard.repository.UserRepository;
+import com.example.myboard.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService{
+	
+	@Autowired
+	private UserRepository user;
+	
+	public UserServiceImpl(UserRepository user) {
+		super();
+		this.user = user;
+	}
+
+	@Override
+	public List<User> getAllUser() {
+		return user.findAll();			// 상속받은 인터페이스의 함수를 오버라이딩
+	}
+	
+}
