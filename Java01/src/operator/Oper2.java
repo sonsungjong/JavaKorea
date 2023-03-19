978500cb70a444a11ad262b0cb5714e68c519342
sonsungjong <tjdwhd70599@gmail.com>
2023-03-19 10:05:17 +0900

    2023.03.18 Java 1일

---
+++ Java01/src/operator/Oper2.java
@@ -0,0 +1,19 @@
+package operator;
+
+public class Oper2 {
+	public static void main(String[] args) {
+		int 국어 = 49;
+		int 영어 = 40;
+		int 수학 = 60;
+		
+		// 합계와 평균을 구하세요
+		
+		/*
+		 합계 : OOO점
+		 평균 : OOO점 
+		*/
+		int 합계 = 국어 + 영어 + 수학;
+		System.out.println("합계 : "+합계);
+		System.out.println("평균 : "+((double)합계/3));
+	}
+}
---
+++ Java01/src/variable/Type2.java
@@ -0,0 +1,22 @@
+package variable;
+
+public class Type2 {
+	public static void main(String[] args) {
+		// 변수를 활용해서
+		// 이름, 출생년도, 키
+		
+		// 예시의 OOO을 변수로 사용
+		/*
+		 이름 : OOO
+		 출생년도 : OOOO년 
+		 키 : OOO.OOcm
+		 */
+		String name = "홍길동";
+		int birth = 2000;
+		double height = 175.5;
+		
+		System.out.println("이름 : "+name);
+		System.out.println("출생년도 : "+birth+"년");
+		System.out.println("키 : "+height+"cm");
+	}
+}
