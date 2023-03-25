72331ac1bd610a47b32e6718b9980c4292eebcba
yonggyo1125 <yonggyo00@kakao.com>
2023-03-25 13:10:02 +0900

    Java 3일 반복문

---
+++ Java03/src/loop/Quiz1.java
@@ -0,0 +1,14 @@
+package loop;
+
+import java.util.Scanner;
+
+public class Quiz1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		// 1. 1~10 중 짝수만 출력하기
+		
+		// 2. 정수형태 비밀번호 맞추기 (기회는 5번, 맞추면 맞췄다!, 기회를 모두 사용하면 접속불가.)
+		
+		
+	}
+}
