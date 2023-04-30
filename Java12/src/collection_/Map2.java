8eb4b570f2e9ca03971a2209841ad65408b5ece2
sonsungjong <tjdwhd70599@gmail.com>
2023-04-30 12:43:06 +0900

    2023-04-30 Java12 컬렉션

---
+++ Java12/src/collection_/Map2.java
@@ -0,0 +1,101 @@
+package collection_;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Map2 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		// 1.추가 2.변경 3.삭제 4.조회 0.끝내기
+		MyDictionary dic = new MyDictionary();		// 생성자에서 flower:꽃, sea:바다, new:새로운
+		String word1 = "", word2 = "";
+		int choice = 0;
+		
+		while(true) {
+			System.out.println("1.추가 2.변경 3.삭제 4.조회 0.끝내기");
+			choice = Integer.parseInt(sc.nextLine());
+			if(choice == 1) {
+				// 1. 추가
+				System.out.print("추가할 영단어를 입력하세요>>");
+				word1 = sc.nextLine();
+				System.out.print("추가할 한국어 뜻을 입력하세요>>");
+				word2 = sc.nextLine();
+				dic.addWord(word1, word2);
+			}else if(choice == 2) {
+				// 2. 변경
+				System.out.print("변경할 영단어를 입력하세요>>");
+				word1 = sc.nextLine();
+				System.out.print("변경 후의 한국어 뜻을 입력하세요>>");
+				word2 = sc.nextLine();
+				dic.modifyWord(word1, word2);		// 뜻을 변경
+			}else if(choice == 3) {
+				// 3. 삭제
+				System.out.println("삭제할 영단어 또는 한국어 뜻을 입력하세요>>");
+				word1 = sc.nextLine();
+				dic.deleteWord(word1);
+			}else if(choice == 4) {
+				// 4. 조회
+				dic.showAll();
+			}else if(choice == 0) {
+				// 0. 끝내기
+				break;
+			}else {
+				/* no actions */
+			}
+		}
+		System.out.println("프로그램 종료");
+		sc.close();
+	}
+}
+
+// 한영사전 만들기
+// 생성자에 단어 3개정도
+class MyDictionary{
+	// 멤버변수 private
+	private Map<String, String> map;		// 다형성 (Map은 HashMap의 부모클래스)
+	
+	// 메서드 public
+	public MyDictionary() {
+		map = new HashMap<String,String>();
+		map.put("flower", "꽃");
+		map.put("sea", "바다");
+		map.put("new", "새로운");
+	}
+	
+	public void addWord(String word1, String word2) {
+		map.put(word1, word2);
+	}
+
+	public void modifyWord(String word1, String word2) {
+		map.put(word1, word2);			// 키가 중복되면 값을 덮어씀
+	}
+
+	// 전체 조회
+	public void showAll() {
+		Set<String> set = map.keySet();
+		for(String e : set) {
+			System.out.println(e);
+		}
+	}
+
+	// 영단어 또는 한국어 뜻을 통해 삭제
+	public void deleteWord(String word) {
+		if(map.containsKey(word)) {
+			map.remove(word);			// 키값을 찾아서 삭제
+		}else if(map.containsValue(word)) {
+			// 값을 통해서 해당하는 키의 위치를 찾고 키값으로 삭제
+			String target = "";
+			Set<String> set = map.keySet();
+
+            for (String e : set) {
+                if (map.get(e).equals(word)) {
+                    target = e;
+                }
+            }
+            map.remove(target);
+		}
+	}
+}
+
---
+++ Java12/src/collection_/Set1.java
@@ -0,0 +1,66 @@
+package collection_;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.TreeSet;
+
+public class Set1 {
+	public static void main(String[] args) {
+		// List, Set, Map
+		// List : 동적 배열 (순서O, 중복O)
+		// Set : List에서 중복이 불가능하게함 (순서X, 중복X)
+		// Map : 방번호를 내가 정함, {키:값, 키:값} (키와 값의 쌍으로 구성)
+		
+		// Set : HashSet(순서X, 중복X), TreeSet(오름차순 정렬, 중복X)
+		// Iterator : Set 전체 사용을 위해서 많이 쓰임 (Set이 순서가 없어서 순서를 할당)
+		// Set : Map을 사용할 때, 중복값을 없애는 기능이 필요할 때
+		HashSet<Integer> hs = new HashSet<>();		// <<Set은 중복이 안된다>>
+		
+		// 추가 add
+		hs.add(1);
+		hs.add(2);
+		hs.add(4);
+		hs.add(1);
+		hs.add(2);
+		hs.add(3);
+		
+		// 전체조회 Iterator
+		Iterator<Integer> it = hs.iterator();
+		while(it.hasNext()) {
+			System.out.println(it.next());
+		}
+		// 1 2 3 4
+		
+		// TreeSet : HashSet + 정렬기능
+		TreeSet<String> ts = new TreeSet<>();		// a b d a b c
+		// 추가 add
+		ts.add("a");
+		ts.add("b");
+		ts.add("d");
+		ts.add("a");
+		ts.add("b");
+		ts.add("c");
+		
+//		Iterator<String> iter = ts.iterator();
+//		while(iter.hasNext()) {
+//			System.out.println(iter.next());
+//		}
+		for(String element : ts) {
+			System.out.println(element);
+		}
+		
+		int 배열[] = {1,2,3,4,5,6,7,8,9};
+		// 전체 조회 (일반for문)
+		// for(int i=0;i<배열.length;++i) {System.out.print(배열[i]+" ");}
+		// for-each (배열전용 for문)
+		for(int 임시 : 배열) {
+			System.out.print(임시+" ");
+		}
+	}
+}
+
+
+
+
+
+
